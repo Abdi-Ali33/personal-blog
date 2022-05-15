5200f338fe3a829c923a7605d2d00f772c5b1842
Abdi Ali <alikune33@gmail.com>
2022-05-15 18:57:13 +0300

    create request.py file

---
+++ app/request.py
@@ -0,0 +1,18 @@
+import requests
+from .models import Quotes
+
+base_url = None
+
+
+def configure_request(app):
+    global base_url
+
+    base_url = app.config['BASE_URL']
+
+
+def get_quotes():
+    data = requests.get('http://quotes.stormconsultancy.co.uk/random.json')
+    response = data.json()
+    results = response
+
+    return results
