9dd96523599504b464f46c397272dce5a4029d72
Abdi Ali <alikune33@gmail.com>
2022-05-15 13:13:21 +0300

    create forms file content inside main folder

---
+++ app/main/forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SubmitField, TextAreaField, RadioField
+from wtforms.validators import Required, Email, EqualTo
+from wtforms import ValidationError
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class CommentForm(FlaskForm):
+    description = TextAreaField('Add comment', validators=[Required()])
+    submit = SubmitField()
+
+
+class BlogForm(FlaskForm):
+
+    title = StringField('Blog title', validators=[Required()])
+    blog = TextAreaField('New blog', validators=[Required()])
+    submit = SubmitField('Submit')
