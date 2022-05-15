f8442ba8d56e27071ab12e69c15df76ee8e0afa8
Abdi Ali <alikune33@gmail.com>
2022-05-15 18:59:48 +0300

    create config.py file

---
+++ config.py
@@ -0,0 +1,54 @@
+import os
+
+
+class Config:
+
+    '''
+    General configuration parent class
+    '''
+    BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # email configs
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+
+class ProdConfig(Config):
+    '''
+    Pruduction configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings 
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:vique@localhost/blog'
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
+}
